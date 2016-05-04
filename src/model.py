9efc91113cde23e3e32772962ed13eb94d0f5b53
JP Colomer <jpcolomer@gmail.com>
2016-05-04 07:33:07 +0000

    Added pad to models

--- src/model.py
+++ src/model.py
@@ -25,23 +25,23 @@ def model_a():
     net = {}
     net['input'] = InputLayer((None, 3, 32, 32))
     #net['drop_in'] =  DropoutLayer(net['input'], p=0.2)
-
-    net['conv1_1'] = ConvLayer(net['input'], num_filters=96, filter_size=5, flip_filters=False)
+    net['conv1_1'] = ConvLayer(net['input'], num_filters=96, filter_size=5, pad=1, flip_filters=False)
 
     net['conv2_1'] = PoolLayer(net['conv1_1'], pool_size=3, stride=2)
     #net['drop2_1'] =  DropoutLayer(net['conv2_1'], p=0.5)
 
 
-    net['conv3_1'] = ConvLayer(net['conv2_1'], num_filters=192, filter_size=5, flip_filters=False)
-    
+    net['conv3_1'] = ConvLayer(net['conv2_1'], num_filters=192, filter_size=5, pad=1, flip_filters=False)
+
     net['conv4_1'] = PoolLayer(net['conv3_1'], pool_size=3, stride=2)
     #net['drop4_1'] =  DropoutLayer(net['conv4_1'], p=0.5)
 
-    net['conv5_1'] = ConvLayer(net['conv4_1'], num_filters=192, filter_size=3, flip_filters=False)
-    
+    net['conv5_1'] = ConvLayer(net['conv4_1'], num_filters=192, filter_size=3, pad=1, flip_filters=False)
+
     net['conv6_1'] = ConvLayer(net['conv5_1'], num_filters=192, filter_size=1, flip_filters=False)
-    net['conv7_1'] = ConvLayer(net['conv6_1'], num_filters=10,  filter_size=1, flip_filters=False)
+    net['conv7_1'] = ConvLayer(net['conv6_1'], num_filters=10, filter_size=1, flip_filters=False)
     net['global_avg'] = GlobalPoolLayer(net['conv7_1'])
+    #net['fc10'] = DenseLayer(net['conv7_1'], num_units=10, nonlinearity=None)
     net['output'] = NonlinearityLayer(net['global_avg'], softmax)
     return net
 
@@ -50,19 +50,19 @@ def model_b():
     net['input'] = InputLayer((None, 3, 32, 32))
     net['drop_in'] =  DropoutLayer(net['input'], p=0.2)
 
-    net['conv1_1'] = ConvLayer(net['drop_in'], num_filters=96, pad=1, filter_size=5, pad=1)
+    net['conv1_1'] = ConvLayer(net['drop_in'], num_filters=96, pad=1, filter_size=5)
     net['conv1_2'] = ConvLayer(net['conv1_1'], num_filters=96, filter_size=1)
 
     net['conv2_1'] = PoolLayer(net['conv1_2'], pool_size=3, stride=2)
     net['drop2_1'] =  DropoutLayer(net['conv2_1'], p=0.5)
 
-    net['conv3_1'] = ConvLayer(net['drop2_1'], num_filters=192, pad=1, filter_size=5, pad=1)
+    net['conv3_1'] = ConvLayer(net['drop2_1'], num_filters=192, pad=1, filter_size=5)
     net['conv3_2'] = ConvLayer(net['conv3_1'], num_filters=192, filter_size=1)
 
     net['conv4_1'] = PoolLayer(net['conv3_2'], pool_size=3, stride=2)
     net['drop4_1'] =  DropoutLayer(net['conv4_1'], p=0.5)
 
-    net['conv5_1'] = ConvLayer(net['drop4_1'], num_filters=192, pad=1, filter_size=3, pad=1)
+    net['conv5_1'] = ConvLayer(net['drop4_1'], num_filters=192, pad=1, filter_size=3)
     net['conv6_1'] = ConvLayer(net['conv5_1'], num_filters=192, filter_size=1)
     net['conv7_1'] = ConvLayer(net['conv6_1'], num_filters=10, filter_size=1)
     net['global_avg'] = GlobalPoolLayer(net['conv7_1'])
